847add325bb0810eedb6069271e71498c5cf4462
JesseWu <wjxdem@gmail.com>
2020-06-02 18:31:17 +0800

    Update commonTable.js

--- components/commonTable/commonTable.js
+++ components/commonTable/commonTable.js
@@ -8,6 +8,39 @@ import './commonTable.less';
  * 2. 数据信息（id， 内容， 回调方法等： 根据列type提供）
  * 3. 页码导航（总条数、当前页码、每页条数）
  * 4. 底部提示信息（提示内容）
+ * 用法示例eg:
+ * <common-table
+      class="table"
+      :columns="columns"
+      :contents="tableData"
+      :pagination="pagination"
+    ></common-table>
+ * columns: [{ prop: 'typeDesc', label: '需求类型', type: 'text',onClick: data => {
+ *           if (data.type === 4) {
+ *             this.$emit('showDrawer', {
+ *               title: '需求变更详情',
+ *               id: data.taskNo
+ *             });
+ *           } else {
+ *             this.$emit('showDrawer', { title: '需求详情', id: data.taskNo });
+ *           }
+ *         }, width: 100 }...],
+ * tableData: [],
+ * pagination: {
+ *       total: 0,
+ *       currentPage: 1,
+ *       currentPageSize: 10,
+ *       currentPageChange: page => {
+ *         this.pagination.currentPage = page;
+ *         this.params.searchFlag = false;
+ *         this.handleflushFilter(this.params);
+ *       },
+ *       sizeChange: size => {
+ *         this.pagination.currentPageSize = size;
+ *         this.params.searchFlag = false;
+ *         this.handleflushFilter(this.params);
+ *       }
+ *     }
  */
 export default {
   name: 'common-table',
